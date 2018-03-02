090da91d0116722f34c6168fba21ea00a742632b
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-02 12:45:29 +0000

    Add display board to game class

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,5 +7,9 @@ describe("Game", function() {
     expect(game.board).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
-
+  it("Game shows a 3 x 3 board", function() {
+    expect(game.displayBoard()).toEqual(1 + " | " + 2 + " | " + 3 + " "
+    + '\n' + '-----------\n' + ' ' + 4 + ' | ' + 5 + ' | ' + 6 + '\n' + '-----------\n'
+  + ' ' + 7 + ' | ' + 8 + ' | ' + 9);
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,9 @@
 function Game() {
   this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
+
+Game.prototype.displayBoard = function() {
+  return 1 + " | " + 2 + " | " + 3 + " "
+  + '\n' + '-----------\n' + ' ' + 4 + ' | ' + 5 + ' | ' + 6
+  + '\n' + '-----------\n' + ' ' + 7 + ' | ' + 8 + ' | ' + 9;
+};
